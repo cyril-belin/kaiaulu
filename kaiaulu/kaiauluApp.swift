a600d5f4b29ad1fe7731bf2962f628515be97621
cyril belin <cyrilbelin75@gmail.com>
2022-12-19 18:54:40 +0100

    Initial Commit

---
+++ kaiaulu/kaiauluApp.swift
@@ -0,0 +1,17 @@
+//
+//  kaiauluApp.swift
+//  kaiaulu
+//
+//  Created by cyril belin on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct kaiauluApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
